f83c5ad3506180288a49f2702c08c56cb3c94c16
Simon <simon.stockinger2019@gmail.com>
2025-01-24 11:50:03 +0100

    Display exercise data multiline

--- components/Exercise.jsx
+++ components/Exercise.jsx
@@ -4,9 +4,9 @@ const Exercise = ({ navigation, exerciseData, dayName, exerciseNumber }) => {
   return (
     <DynamicButton
       buttonSymbol={{ name: 'edit', size: 24 }}
-      labelText={`${exerciseData.name || ''} - ${exerciseData.sets || 0} x ${
+      labelText={`${exerciseData.name || ''}\n${exerciseData.sets || 0} x ${
         exerciseData.reps || 0
-      } - ${exerciseData.weight || 0}kg`}
+      }\n${exerciseData.weight || 0}kg`}
       onPress={() => {
         navigation.navigate('Exercise Input', {
           dayName: dayName,
